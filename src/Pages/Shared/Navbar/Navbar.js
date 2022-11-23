408257f8bf627607f3ea4d5614402a807a986100
mohi14 <mohiuddinkhan1409@gmail.com>
2022-11-24 03:34:58 +0600

    navbar done

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { TbShoppingCart } from "react-icons/tb";
+import { AuthContext } from '../../../contexts/AuthProvider';
+import { CgProfile } from "react-icons/cg";
 
 const Navbar = () => {
+    const { user, LogOut } = useContext(AuthContext)
+
+    const menuItems = <React.Fragment>
+        <li><NavLink to='/home'>Home</NavLink></li>
+        <li><NavLink to='/blog'>Blog</NavLink></li>
+
+
+        {
+            user?.uid ?
+                <>
+                    <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
+                    <li><button>Sign Out</button></li>
+                </>
+                :
+                <li><Link to='/home'>Login</Link></li>
+        }
+
+    </React.Fragment>
     return (
-        <div>
-            This is navbar
+        <div className="navbar bg-base-100 shadow-xl sticky  py-6">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to='/' className="btn btn-ghost normal-case text-xl lg:text-4xl lg:ml-[200px] font-bold text-neutral">C<TbShoppingCart className='text-primary' />MERCIO</Link>
+            </div>
+
+            <div className="navbar-end lg:mr-[200px] ">
+                <div className=" hidden lg:flex text-xl font-semibold mr-3">
+                    <ul className="menu menu-horizontal p-0">
+                        {menuItems}
+                    </ul>
+                </div>
+                <div >
+                    {
+                        user?.uid ?
+                            <>{
+                                user?.photoURL ?
+                                    <>
+                                        <div className="avatar">
+                                            <div className="w-24 rounded-full">
+                                                <img src={user?.photoURL} alt='' />
+                                            </div>
+                                        </div>
+                                    </>
+                                    :
+                                    <CgProfile className='text-3xl font-semibold' />
+                            }
+                            </>
+                            : ''
+                    }
+
+                </div>
+            </div>
         </div>
     );
 };
