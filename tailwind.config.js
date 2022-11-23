22c834a43f8d69513def15092c8fc4a4b5834083
mohi14 <mohiuddinkhan1409@gmail.com>
2022-11-24 01:26:22 +0600

    project setup and favicon done

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        coomerciotheme: {
+          primary: "#F28C18",
+          secondary: "#6D3A9C",
+          accent: "#51A800",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
